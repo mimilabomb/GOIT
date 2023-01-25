26143f58d7c8fb1f147b56e6f843300591cf337c
Bartosz Szymański <bartoszsz13@gmail.com>
2023-01-25 19:36:47 +0100

    Pierwszy commit

---
+++ script.js
@@ -0,0 +1,9 @@
+function myFunction() {
+    const element = document.getElementById("id01");
+    element.innerHTML = "New Heading";
+}
+
+function myColorChange() {
+    const element = document.getElementById("id01");
+    element.style.color = "red";
+}
