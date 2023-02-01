38080cf945e1a16a2e2d9ae997f230cdc6810cba
Bartosz Szymański <bartoszsz13@gmail.com>
2023-02-01 21:04:19 +0100

    Zajecia 5 branch

--- script.js
+++ script.js
@@ -167,3 +167,76 @@ function mojaFunkcja(id) {
     element2.innerHTML = informacja;
   }
 }
+
+function greet(name) {
+  console.log(`Pozdrawiam ${name}`);
+  return `Pozdrawiam ${name}.`;
+}
+
+function dodawanie(a, b, c) {
+  return a + b + c;
+}
+
+function greet(name2) {
+  console.log(`Pozdrawiam ${name2}.`);
+  return name2;
+}
+
+function registerGuest(name, parametr) {
+  console.log(`Rejestracja gościa ${name}.`);
+
+  parametr(name);
+}
+
+greet("Kiwi");
+console.log(greet("Kiwi"));
+console.log(greet);
+
+registerGuest("Mango", greet);
+
+function processCall(recipient) {
+  const liczba = Math.random() > 0.5;
+
+  console.log(liczba);
+  console.log(!liczba);
+
+  if (!liczba) {
+    console.log(
+      "Abonent " + recipient + " jest niedostępny, zostaw wiadomość."
+    );
+  } else {
+    console.log(`Łączenie z ${recipient}, proszę czekać...`);
+  }
+}
+
+processCall("Mango");
+
+function processCall1(recipient, onAvailable, onNotAvailable) {
+  // Symuluj dostępność abonenta za pomocą liczby losowej
+  const isRecipientAvailable = Math.random() > 0.5;
+
+  if (!isRecipientAvailable) {
+    onNotAvailable(recipient);
+    return;
+  }
+
+  onAvailable(recipient);
+}
+
+function takeCall(name) {
+  console.log(`Łączenie z ${name}, proszę czekać...`);
+  // Logika odbierania połączenia
+}
+
+function activateAnsweringMachine(name) {
+  console.log(`Abonent ${name} jest niedostępny, zostaw wiadomość.`);
+  // Logika aktywacji automatycznej sekretarki
+}
+
+function leaveHoloMessage(name) {
+  console.log(`Abonent ${name} jest niedostępny, nagrywamy hologram.`);
+  // Logika nagrywania hologramu
+}
+
+processCall1("Mango", takeCall, activateAnsweringMachine);
+processCall1("Poly", takeCall, leaveHoloMessage);
